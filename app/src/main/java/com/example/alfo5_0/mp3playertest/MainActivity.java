c1ee290cb0dd6e7ba613e1516f8550baa597f70e
rlaeogus0911 <rlaeogus0911@nate.com>
2019-12-05 15:21:47 +0900

    mp3 프로젝트 자료임

---
+++ app/src/main/java/com/example/alfo5_0/mp3playertest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.alfo5_0.mp3playertest;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    private Switch switchMusic;
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        switchMusic=findViewById(R.id.switchMusic);
+        //음악을 들을 곡을 선택한 미디어 플레이어 객체를 만듬
+        mediaPlayer=MediaPlayer.create(this,R.raw.love_poem);
+
+        switchMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(switchMusic.isChecked()==true){
+                    mediaPlayer.start();
+                }else{
+                    mediaPlayer.stop();
+                }
+            }
+        });
+    }
+}
+
+
+
+
+
